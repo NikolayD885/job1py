4bc88dc1c1c6cb1102d6f8c8bef0aee7b71bb6e6
NikolayD <kolik885@ya.ru>
2022-10-03 22:05:00 +0700

    Программа №5 урок 1

---
+++ 005.py
@@ -0,0 +1,14 @@
+# Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+
+trigger = True
+
+for x in [True, False]:
+    for y in [True, False]:
+        for z in [True, False]:
+            if not (x or y or z) != (not x and not y and not z):
+                print('Не верно')
+                trigger = False
+                break
+
+if trigger:
+    print('Выражение верно')
